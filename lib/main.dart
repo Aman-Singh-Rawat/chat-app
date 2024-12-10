fd39bde80630c1fc6269d7517cf34ecd0c4426a5
Aman Singh <amanrawat6767@gmail.com>
2024-12-10 11:17:03 +0530

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:chat_app/screens/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,7 +23,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(
               seedColor: const Color.fromARGB(255, 63, 17, 177),)
         ),
-        home: AuthScreen(),
+        home: const AuthScreen(),
     );
   }
 }
